5114693f43fd1c227ea6f6583ee012763522eddc
automathematical <vanleemputtenmathieu@gmail.com>
2023-06-22 13:02:10 +0200

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BuildingViewer } from './components/building-viewer'
+import { MapViewer } from './components/map-viewer'
+import { LoginForm } from './components/login-form'
+
 function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/building"
+          element={<BuildingViewer />}
+        />
+        <Route
+          path="/map"
+          element={<MapViewer />}
+        />
+        <Route
+          path="/login"
+          element={<LoginForm />}
+        />
+        <Route
+          path="/"
+          element={<LoginForm />}
+        />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { initializeApp } from 'firebase/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAgOtT8htmoycbvZ82FMrsX44DZ-E-bX8',
+  authDomain: 'ifc-frontend-course.firebaseapp.com',
+  projectId: 'ifc-frontend-course',
+  storageBucket: 'ifc-frontend-course.appspot.com',
+  messagingSenderId: '876378243167',
+  appId: '1:876378243167:web:6a4888223d4da8370b9bea',
+}
+
+initializeApp(firebaseConfig)
+
 root.render(
   <React.StrictMode>
     <App />
